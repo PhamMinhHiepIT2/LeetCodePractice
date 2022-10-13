5c04137606e2762abe06b7153c157b11014ee5ae
Pham Minh Hiep <v.hieppm1@vinbigdata.com>
2022-10-13 09:49:58 +0700

    update remove_k_digit

--- remove_k_digit.py
+++ remove_k_digit.py
@@ -3,33 +3,33 @@ class Solution:
         n = len(num)
         st = []
         if k >= n:
-            return 0
+            return "0"
         if list(set(num))[0] == '0':
-            return 0
+            return "0"
 
         for i in range(n):
             while st and k > 0 and num[i] < st[-1]:
                 st.pop()
                 k -= 1
 
-            if st and num[i] != '0':
+            if st or num[i] != '0':
                 st.append(num[i])
 
         while st and k > 0:
             st.pop()
             k -= 1
             if len(st) == 0:
-                return 0
+                return "0"
         if len(st) == 0:
-            return 0
+            return "0"
         res = ""
         while st:
-            res += st.pop()
+            res += st.pop(0)
 
-        print(res)
+        return res
 
 
 if __name__ == "__main__":
-    num = "1432219"
+    num = "1132219"
     k = 3
     print(Solution().removeKdigits(num, k))
