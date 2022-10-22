3eaa5877b412882bf83fd5a06fe6cead3e8d6bb3
Pham Minh Hiep <v.hieppm1@vinbigdata.com>
2022-10-22 15:03:20 +0700

    debt

--- 257_binary_tree_paths.py
+++ 257_binary_tree_paths.py
@@ -1,5 +1,4 @@
-from typing import Optional, List
-
+from typing import List, Optional
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -11,49 +10,21 @@ class TreeNode:
 class Solution:
     def __init__(self) -> None:
         self.p = ""
-        self.cur_p = ""
         self.paths = []
 
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        if root.left is None and root.right is None:
-            print("Current root: {}".format(root))
-            self.p += str(root.val)
-            self.paths.append(self.p)
-            print("Current p: {}".format(self.p))
-            self.p = ""
-            return list(set(self.paths))
-
-        elif root.left != None and root.right == None:
-            print("Go to left leaf")
-            print("Current root: {}".format(root))
-            self.p += (str(root.val) + "->")
-            self.binaryTreePaths(root.left)
-            print("Current p: {}".format(self.p))
-        elif root.left == None and root.right != None:
-            print("Go to right leaf")
-            print("Current root: {}".format(root))
-            self.p += (str(root.val) + "->")
-            self.binaryTreePaths(root.right)
-            print("Current p: {}".format(self.p))
-        else:
-            self.cur_p = self.p
-            self.p = ""
-            left_res = self.binaryTreePaths(root.left)
-            print("Left res: {}".format(left_res))
-            right_res = self.binaryTreePaths(root.right)
-            print("Right res: {}".format(right_res))
-            self.paths = merge_2_side(
-                left_res, right_res, root.val, self.cur_p)
-            return list(set(self.paths))
-        # return list(set(self.paths))
+        return dfs(root, self.p, self.paths)
 
 
-def merge_2_side(left_res: list, right_res: list, root: int, p: str):
-    print("Merging {} and {}".format(left_res, right_res))
-    res = left_res + right_res
-    new_res = []
-    for el in res:
-        new_el = str(root) + "->" + el
-        # new_el = p + new_el
-        new_res.append(new_el)
-    return new_res
+def dfs(root: Optional[TreeNode], p, res):
+    if not root.left and not root.right:
+        p += str(root.val)
+        res.append(p)
+
+    p += str(root.val) + "->"
+
+    if root.left:
+        dfs(root.left, p, res)
+    if root.right:
+        dfs(root.right, p, res)
+    return res
